1b087a95c242c264cecc3ba306663fd69ca27c2a
Francesco <lombardi.francesco09@gmail.com>
2026-04-12 23:56:05 +0200

    Android project

---
+++ app/src/main/java/com/example/myjavaandroidapp/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.myjavaandroidapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.myjavaandroidapp.fragments.HomeFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
+    }
+
+}
---
+++ app/src/main/java/com/example/myjavaandroidapp/fragments/HomeFragment.java
@@ -0,0 +1,13 @@
+package com.example.myjavaandroidapp.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.myjavaandroidapp.R;
+
+public class HomeFragment extends Fragment {
+
+    public HomeFragment() {
+        super(R.layout.fragment_home);
+    }
+
+}
